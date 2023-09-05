1105b2e6f0021288a01db33242e6a75b9829ec97
judy-chelangat <judysigilai75@gmail.com>
2023-09-05 11:17:52 +0300

    created a session and populated farmers table with fake data

--- lib/seed.py
+++ lib/seed.py
@@ -4,4 +4,33 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Consumer,Farmer,Produce,Orders
+from models import Consumer,Farmer,Produce,Order
+
+fake=Faker()
+
+if __name__=='__main__':
+    engine= create_engine('sqlite:///farmers.db')
+    Session = sessionmaker(bind=engine) #creating a session
+    session = Session()
+    session.query(Consumer).delete() #to delete the previous records to avoid duplication
+    session.query(Farmer).delete()
+    session.query(Produce).delete()
+    session.query(Order).delete()
+
+
+print("Seeding Farmers")
+#populating the farmer table with few farmers
+
+
+farmers = [
+    Farmer(
+        name=fake.name(),
+        email=fake.email(),
+        phone_number= fake.phone_number(),
+        city=fake.city()
+    )
+for i in range(10)] #creating 10 records 
+
+#adding the session
+session.add_all(farmers)
+session.commit()
