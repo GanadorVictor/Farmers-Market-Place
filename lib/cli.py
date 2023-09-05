e61445046ac41d3f5b99ae63a37b68a4967c1cb8
judy-chelangat <judysigilai75@gmail.com>
2023-09-05 14:45:16 +0300

    created a session

--- lib/cli.py
+++ lib/cli.py
@@ -3,3 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Farmer,Produce,Order,Consumer
 
+# define the database connection 
+DATABASE_URI = 'sqlite:///restuarants.db' #path to the database
+
+engine = create_engine(DATABASE_URI,echo=False)
+ #creating the engine
+Session = sessionmaker(bind=engine) #creating a session
+session = Session()
